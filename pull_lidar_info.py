dc250e218b82ad244b4a8df031f74a9438c15656
theohartsook <53097335+theohartsook@users.noreply.github.com>
2019-11-24 10:33:03 -0800

    Update pull_lidar_info.py
    Updated to loop through a folder

--- pull_lidar_info.py
+++ pull_lidar_info.py
@@ -1,22 +1,30 @@
+import os
+import subprocess
 import csv
 
-filepath = '/home/theo/Desktop/P406_sfmuav_unregistered_group1_densified_point_cloud_part_15_info.txt'
+directory = '/home/theo/Desktop/1_Combined'
 plot_reference_table = '/home/theo/Desktop/plot_info_reference.csv'
-with open(filepath) as f:
-    for x in f:
-        if "GTCitationGeoKey" in x:
-            if "WGS 84" in x:
-                wgs = "84"
-            if "UTM zone 10N" in x:
-                utm = "10n"
-            if "UTM zone 11N" in x:
-                utm = "11n"
-            with open(plot_reference_table, 'a') as ref:
-                ref_headers = ['wgs', 'utm']
-                ref_row = [wgs, utm]
-                writer = csv.DictWriter(ref, fieldnames=ref_headers)
-                writer.writeheader()
-                writer.writerow({'wgs' : wgs, 'utm' : utm})
-            ref.close()
-f.close()
-
+for filename in os.listdir(directory):
+    if filename.endswith(".las"):
+        las_file = directory + '/' + filename
+        subprocess.call(["lasinfo", "-i", las_file, "-o", "temp_info.txt", "-odir", directory])
+        with open(directory + "/temp_info.txt") as f:
+            plot_id=filename[0:4]
+            for x in f:
+                if "GTCitationGeoKey" in x:
+                    if "WGS 84" in x:
+                        wgs = "84"
+                    if "UTM zone 10N" in x:
+                        utm = "10n"
+                    if "UTM zone 11N" in x:
+                        utm = "11n"
+                with open(plot_reference_table, 'a') as ref:
+                    ref_headers = ['plot_id', 'wgs', 'utm']
+                    ref_row = [plot_id, wgs, utm]
+                    writer = csv.DictWriter(ref, fieldnames=ref_headers)
+                    writer.writeheader()
+                    writer.writerow({'plot_id' : plot_id, 'wgs' : wgs, 'utm' : utm})
+                ref.close()
+        f.close()
+    else:
+        continue
