4d95dd60ef4e731d85a41f28e0cbd36a4f83678c
theohartsook <53097335+theohartsook@users.noreply.github.com>
2020-01-11 10:37:51 -0800

    Update als_multiprocessing_hpc.py

--- als_multiprocessing_hpc.py
+++ als_multiprocessing_hpc.py
@@ -64,22 +64,22 @@ def tile_processing(tile):
 
 # Get height
     print("height")
-    las_file = temp_directory + "/" + tile[:-4] + "_ground.las"
+    las_file = temp_directory + "/" + tile[:-4] + "_denoised_ground.las"
     subprocess.call(["singularity", "exec", lastools_singularity, "lasheight", "-i", las_file, "-drop_below", "0", "-drop_above", "100", "-odir", temp_directory, "-odix", "_height", "-cpu64"])
 
 # Classify remaining points
     print("classify")
-    las_file = temp_directory + "/" + tile[:-4] + "_height.las"
+    las_file = temp_directory + "/" + tile[:-4] + "_denoised_ground_height.las"
     subprocess.call(["singularity", "exec", lastools_singularity, "lasclassify", "-i", las_file, "-ground_offset", "0.2", "-odir", output_directory, "-odix", "_buffered", "-cpu64"])
 
 # Create seamless tiles
     print("seamless")
-    las_file = temp_directory + "/" + tile[:-4] + "_buffered.las"
+    las_file = output_directory + "/" + tile[:-4] + "_denoised_ground_height_buffered.las"
     subprocess.call(["singularity", "exec", lastools_singularity, "lastile", "-i", las_file, "-remove_buffer", "-odir", output_directory, "-odix", "_seamless", "-cpu64"])
 
 # Clip to plot
     print("plot clip")
-    las_file = temp_directory + "/" + tile[:-4] + "_buffered.las"
+    las_file = output_directory + "/" + tile[:-4] + "_denoised_ground_height_buffered.las"
     plot_id=filename[0:4] # multiple scans don't matter for clipping to plot
     shapefile = shapefile_directory + "/Plot_Centers_Poly_Plot_ID_" + plot_id + ".shp"
     subprocess.call(["singularity", "exec", lastools_singularity, "lasclip", "-i", las_file, "-poly", shapefile, "-odir", output_directory, "-odix", plot_id, "-cpu64"])
