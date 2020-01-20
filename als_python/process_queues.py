f2aca604977624f08e9aac0dfa9d57690cc04b21
theohartsook <53097335+theohartsook@users.noreply.github.com>
2020-01-20 10:28:26 -0800

    Update process_queues.py
    Added DSMs

--- als_python/process_queues.py
+++ als_python/process_queues.py
@@ -1,6 +1,7 @@
 from multiprocessing import Pool, Process, Queue
 from flightlines_to_tiles import flightlines_to_tiles
 from tile_processing import tile_processing
+from dsm_processing import make_tile_dsm
 
 def flight_queue(q, input_directory, lastools_singularity, overwrite = False):
     while not q.empty():
@@ -24,3 +25,17 @@ def tile_queue(q, input_directory, output_directory, lastools_singularity, overw
             print(val_error)
         except Exception as error:
             print(error)
+
+# -------------------
+
+def dsm_queue(q, output_directory, lastools_singularity, overwrite = False):
+    while not q.empty():
+        try:
+            tile = q.get()
+            make_tile_dsm(tile, output_directory, lastools_singularity)
+        except ValueError as val_error:
+            print(val_error)
+        except Exception as error:
+            print(error)
+
+# -------------------
